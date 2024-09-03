ac36b182f22391c3ccfe23a6dccd25fc181c3d53
Thiago Kachinsky <thiagokachidev@gmail.com>
2024-09-03 10:26:20 -0300

    config

--- src/config/cache.ts
+++ src/config/cache.ts
@@ -13,6 +13,7 @@ export default {
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
       password: process.env.REDIS_PASS || undefined,
+      family: 6
     }
   },
   driver: 'redis',
